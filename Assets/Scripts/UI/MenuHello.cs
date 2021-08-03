b027ec610e71aa54995ca371df579ca327eb09a2
koker007 <55190926+koker007@users.noreply.github.com>
2021-08-03 14:10:39 +0500

    add controller hello menu

---
+++ Assets/Scripts/UI/MenuHello.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Семен
+/// <summary>
+/// Контроллер приветственного меню
+/// </summary>
+public class MenuHello : MonoBehaviour
+{
+
+
+
+    //Время когда в последний раз работало это меню
+    float timeLastWork;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Начать игру войти в главное меню
+    /// </summary>
+    public void clickButtonPlay() {
+        
+    }
+
+    /// <summary>
+    /// Сохранить весь игровой прогресс
+    /// </summary>
+    public void clickButtonSave() {
+    
+    }
+
+    /// <summary>
+    /// Открыть настройки меню
+    /// </summary>
+    public void clickButtonOpttion()
+    {
+
+    }
+}
