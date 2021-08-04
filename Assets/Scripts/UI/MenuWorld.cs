96ef439e44b74ac3ee93cc26de4c4ced18981b51
koker007 <55190926+koker007@users.noreply.github.com>
2021-08-04 12:46:25 +0500

    UI save

--- Assets/Scripts/UI/MenuWorld.cs
+++ Assets/Scripts/UI/MenuWorld.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Семен
+/// <summary>
+/// Контролирует UI главного меню
+/// </summary>
 public class MenuWorld : MonoBehaviour
 {
+    static public MenuWorld main;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        main = this;
     }
 
     // Update is called once per frame
