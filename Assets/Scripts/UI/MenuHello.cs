6600bb77858c875eb5a130c80c30e29dbfd18e88
koker007 <55190926+koker007@users.noreply.github.com>
2021-08-04 12:11:22 +0500

    UI CTRL work 1

--- Assets/Scripts/UI/MenuHello.cs
+++ Assets/Scripts/UI/MenuHello.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 //Семен
 /// <summary>
 /// Контроллер приветственного меню
 /// </summary>
 public class MenuHello : MonoBehaviour
 {
+    static public MenuHello main;
 
+    [SerializeField]
+    Text[] Version;
+
+    [SerializeField]
+    GameObject options;
+
+
+    bool isNeedOpenOptions; //Нужно ли открыть или закрыть панель опций
+    
 
 
     //Время когда в последний раз работало это меню
@@ -18,20 +28,20 @@ public class MenuHello : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        main = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdatePanelOpttion();
     }
 
     /// <summary>
     /// Начать игру войти в главное меню
     /// </summary>
     public void clickButtonPlay() {
-        
+        UICTRL.main.OpenWorld();
     }
 
     /// <summary>
@@ -46,6 +56,20 @@ public class MenuHello : MonoBehaviour
     /// </summary>
     public void clickButtonOpttion()
     {
+        isNeedOpenOptions = !isNeedOpenOptions;
+    }
 
+
+    /// <summary>
+    /// Отвечает за состояние панели опций
+    /// </summary>
+    void UpdatePanelOpttion() {
+        if (isNeedOpenOptions && !options.activeSelf)
+        {
+            options.SetActive(true);
+        }
+        else if(!isNeedOpenOptions && options.activeSelf) {
+            options.SetActive(false);
+        }
     }
 }
