0e3f2e3ab43e7d035cd5809246edd9a3dab016f0
Skrylller <77968100+Skrylller@users.noreply.github.com>
2021-09-30 15:11:51 +0500

    lvl 11 -20

--- Assets/Scripts/Global/PlayerProfile.cs
+++ Assets/Scripts/Global/PlayerProfile.cs
@@ -70,7 +70,7 @@ public class PlayerProfile : MonoBehaviour
         ProfileLevel = PlayerPrefs.GetInt("ProfileLevel", 1);
         ProfileScore = PlayerPrefs.GetInt("ProfileScore", 0);
         ProfilelevelOpen = PlayerPrefs.GetInt("ProfielevelOpen", 1);
-        ProfilelevelOpen = 10;
+        ProfilelevelOpen = 100;
 
         GoldAmount = PlayerPrefs.GetInt("GoldAmount", 10);
 
