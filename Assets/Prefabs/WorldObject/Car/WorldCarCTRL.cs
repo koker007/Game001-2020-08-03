0a7f94837f51bdcae9e965581054c99adff04f9e
koker007 <55190926+koker007@users.noreply.github.com>
2021-10-18 16:37:37 +0500

    location china

---
+++ Assets/Prefabs/WorldObject/Car/WorldCarCTRL.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCarCTRL : MonoBehaviour
+{
+
+    [SerializeField]
+    float timeAnimationPlay = 0;
+    [SerializeField]
+    Animation animation;
+
+    [SerializeField]
+    GameObject[] models;
+
+    [SerializeField]
+    float[] veroatnost;
+
+
+    //Рандомизировать отображение модели
+    public void RandomModel() {
+        //Сначало все скрыть
+        foreach (GameObject model in models) {
+            model.SetActive(false);
+        }
+
+        for (int numTest = 0; numTest < 10; numTest++) {
+            //Выбираем модель если успех то выходим
+            if (SelectModel()) {
+                break;
+            }
+        }
+        
+
+        bool SelectModel() {
+            bool selected = false;
+
+            //Выбираем рандомно модель
+            int modelNum = Random.Range(0, models.Length);
+            float veroatnostNow = veroatnost[modelNum];
+
+            //Проверяем текущий шанс появления
+            if (veroatnostNow < Random.Range(0, 100)) {
+                models[modelNum].SetActive(true);
+                selected = true;
+            }
+
+            return selected;
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        AnimationStart();
+        RandomModel();
+    }
+
+    void AnimationStart() {
+        foreach (AnimationState state in animation) {
+            state.time = timeAnimationPlay;
+        }
+
+        animation.Play();
+    }
+
+    public void AnimationRestart() {
+        foreach (AnimationState state in animation)
+        {
+            state.time = 0;
+        }
+
+        animation.Play();
+    }
+}
