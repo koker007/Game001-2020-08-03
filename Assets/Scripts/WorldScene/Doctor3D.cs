2d30cff0106dbf55adfee95ae913a1362855e54c
Skrylller <77968100+Skrylller@users.noreply.github.com>
2022-02-03 16:31:49 +0500

    Doctor3D

---
+++ Assets/Scripts/WorldScene/Doctor3D.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doctor3D : MonoBehaviour
+{
+    public enum DoctorEmotions
+    {
+        def,
+        em1,
+        em2
+    }
+
+    private DoctorEmotions emotion;
+
+
+}
