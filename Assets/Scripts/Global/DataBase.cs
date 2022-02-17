8acc7da760dcfd774159063af797fab84f11c2be
koker007 <55190926+koker007@users.noreply.github.com>
2022-02-17 16:51:47 +0500

    bug fix Firebase

--- Assets/Scripts/Global/DataBase.cs
+++ Assets/Scripts/Global/DataBase.cs
@@ -8,14 +8,20 @@ public class DataBase : MonoBehaviour
 
     //Имена сохраняемых данных, должно совпадать на firebase
 
+    public struct TypeTime{
+        public const string Year = "Year"; //год
+        public const string Month = "Month"; //месяц
+    }
+
     //Раздел уровней
     public struct TypeLevel {
         public const string Levels = "Levels"; //Подраздел уровни
         public const string MoveToWin = "Move_To_Win";
         public const string PlayersVictory = "Players_Passed";
         public const string PlayersNotPassed = "Players_Not_Passed";
+        public const string AverageScore = "Average_Score"; //Количество набираемых очков в среднем
 
-
+        //Покупки общие на уровне
         public const string Store = "Store"; //Покупки - подраздел
         public const string CoronaUse = "Corona_Use"; //корона
         public const string CoronaBuy = "Corona_Buy";
@@ -43,7 +49,9 @@ public class DataBase : MonoBehaviour
     //Раздел профиля
     public struct TypeProfile {
         public const string Profiles = "Profiles";
-        public const string LevelMax = "";
+        public const string Level_Max = "Level_opened_maximum";
+        public const string Level_Open_Averade = "Level_opened_average";
+        public const string Level_Open_Averade_Count = "Level_opened_average_count";
     }
 
     // Start is called before the first frame update
